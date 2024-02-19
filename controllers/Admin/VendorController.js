77cd49a3a6cf8218d892e9f1fbba02dfbecf4060
shoaibzaak <shoaibzaki84@gmail.com>
2024-02-19 13:48:36 +0500

    change in the vendor controller

--- controllers/Admin/VendorController.js
+++ controllers/Admin/VendorController.js
@@ -5,104 +5,129 @@ const Message = require("../../Message");
 const responseHelper = require("../../helper/response.helper");
 const Status = require("../../status");
 const catchAsync = require("../../utils/catchAsync");
-const bcrypt = require("bcrypt");
+const referralCodes = require("referral-codes");
 const Validation = require("../../utils/validations/validation");
-
+const Services=require("../../services")
 
 module.exports = {
   // Retrieve Vendor user by VendorId
   getVendorUser: catchAsync(async (req, res, next) => {
-     // Extract the VendorId from the request parameters
-  const  vendorId  = req.params.id;
-  // Perform a database query to find the vendor user by VendorId
-  const vendorUser = await Model.Vendor.findById( vendorId );
+    // Extract the VendorId from the request parameters
+    const vendorId = req.params.id;
+    // Perform a database query to find the vendor user by VendorId
+    const vendorUser = await Model.Vendor.findById(vendorId);
 
-  // Check if the vendor user is found
-  if (!vendorUser) {
-    return res.status(404).json({ message: 'Vendor user not found' });
-  }
+    // Check if the vendor user is found
+    if (!vendorUser) {
+      return res.status(404).json({ message: "Vendor user not found" });
+    }
 
-  const message = "Vendor created successfully";
-  res.ok(message, vendorUser);
+    const message = "Vendor created successfully";
+    res.ok(message, vendorUser);
   }),
 
   // Create a new Vendor
   createVendor: catchAsync(async (req, res, next) => {
     try {
-        const { firstName, lastName, email } = req.body;
+      const { firstName, lastName, email } = req.body;
 
-        // Default dummy password for all vendors
-        const dummyPassword = "dummyVendorPassword";
+      // Email validation
+      if (!Validation.validateEmail(email)) {
+        return res.badRequest("Invalid email format");
+      }
+      const verifyEmail = await Model.Vendor.findOne({ email });
 
-        // Email validation
-        if (!Validation.validateEmail(email)) {
-            return res.badRequest("Invalid email format");
-        }
+      if (verifyEmail) {
+        throw new HTTPError(Status.BAD_REQUEST, Message.emailAlreadyExists);
+      }
+      const tempPassword = referralCodes.generate({
+        length: 8,
+        charset: referralCodes.charset("dummyPassword"),
+      })[0];
+      console.log();
+      const vendor = new Model.Vendor({
+        firstName,
+        lastName,
+        email,
+        password: tempPassword,
+      });
 
-        const hash = bcrypt.hashSync(dummyPassword, 10);
-        const verifyEmail = await Model.Vendor.findOne({ email });
+      // await vendor.save();
+      const emailMessage = `
+Dear ${firstName} ${lastName},
 
-        if (verifyEmail) {
-            throw new HTTPError(Status.BAD_REQUEST, Message.emailAlreadyExists);
-        }
+You are added by admin of MATT.
 
-        const vendor = new Model.Vendor({
-            firstName,
-            lastName,
-            email,
-            password: hash,
-        });
+Your temporary password is: ${tempPassword}
 
-        await vendor.save();
+Use your temporary password ${tempPassword} and gmail ${email} to login
 
-        return res.ok("Vendor registered successfully.", vendor);
+Best regards,
+The MATT Team
+`;
+
+      // Send the email with the message directly
+      await Services.EmailService.sendEmail(
+        emailMessage,
+        email,
+        email,
+        "User Account Email Verification | vagabond"
+      );
+
+      return res.ok("Vendor registered successfully .", vendor);
     } catch (err) {
-        next(err);
+      next(err);
     }
   }),
 
- // Get all Vendor users with full details
-getAllVendorUsers: catchAsync(async (req, res, next) => {
-  console.log("Vendordetails is called");
-  try {
-    // Fetch all Vendors without pagination
-    const vendors = await Model.Vendor.find({isDeleted:false}).sort("-_id");
-
-    const vendorSize = vendors.length;
-
-    const result = {
-      Vendor: vendors,
-      count: vendorSize,
-    };
-
-    // Check if no Vendors are found
-    if (vendorSize === 0) {
-      res.notFound("no record found",result.Vendor)
-    }
-
-    // Return a success response with the result
-    return responseHelper.success(res, result, "Vendordetails found successfully");
-  } catch (error) {
-    // Handle errors and return a failure response
-    responseHelper.requestfailure(res, error);
-  }
-}),
-
-
-updateVendor: catchAsync(async (req, res, next) => {
+  // Get all Vendor users with full details
+  getAllVendorUsers: catchAsync(async (req, res, next) => {
+    console.log("Vendordetails is called");
     try {
+      // Fetch all Vendors without pagination
+      const vendors = await Model.Vendor.find({ isDeleted: false }).sort(
+        "-_id"
+      );
 
-       // Get the Vendor user data from the request body
+      const vendorSize = vendors.length;
+
+      const result = {
+        Vendor: vendors,
+        count: vendorSize,
+      };
+
+      // Check if no Vendors are found
+      if (vendorSize === 0) {
+        res.notFound("no record found", result.Vendor);
+      }
+
+      // Return a success response with the result
+      return responseHelper.success(
+        res,
+        result,
+        "Vendordetails found successfully"
+      );
+    } catch (error) {
+      // Handle errors and return a failure response
+      responseHelper.requestfailure(res, error);
+    }
+  }),
+
+  updateVendor: catchAsync(async (req, res, next) => {
+    try {
+      // Get the Vendor user data from the request body
       const vendorUserData = req.body;
-  
+
       // Find the Vendor by ID
-      const existingVendor = await Model.Vendor.findById(vendorUserData.vendorId);
-  
+      const existingVendor = await Model.Vendor.findById(
+        vendorUserData.vendorId
+      );
+
       // Check if the Vendor exists
       if (!existingVendor) {
         throw new HTTPError(Status.NOT_FOUND, "Vendor not found");
       }
-  
+
       // Update the Vendor user with the updated data
       const result = await Model.Vendor.findByIdAndUpdate(
         vendorUserData.vendorId,
@@ -111,33 +136,33 @@ updateVendor: catchAsync(async (req, res, next) => {
           new: true,
         }
       );
-  
+
       const message = "Vendor status updated successfully";
       res.ok(message, result);
     } catch (err) {
       next(err); // Pass the error to the error-handling middleware
     }
   }),
-  
+
   // Delete a Vendor user
   declineVendor: catchAsync(async (req, res, next) => {
     var vendorId = req.params.id;
     try {
-        // Find the Vendor by ID and update it to set isDeleted to true
-        const updatedVendor = await Model.Vendor.findByIdAndUpdate(
-            vendorId,
-            { isDeleted: true },
-            { new: true } // To return the updated document
-        );
+      // Find the Vendor by ID and update it to set isDeleted to true
+      const updatedVendor = await Model.Vendor.findByIdAndUpdate(
+        vendorId,
+        { isDeleted: true },
+        { new: true } // To return the updated document
+      );
 
-        // If Vendor is not found, return a bad request response
-        if (!updatedVendor)
-            return res.badRequest("Vendor not found in our records");
+      // If Vendor is not found, return a bad request response
+      if (!updatedVendor)
+        return res.badRequest("Vendor not found in our records");
 
-        var message = "Vendor deleted successfully";
-        res.ok(message, updatedVendor);
+      var message = "Vendor deleted successfully";
+      res.ok(message, updatedVendor);
     } catch (err) {
-        throw new HTTPError(Status.INTERNAL_SERVER_ERROR, err);
+      throw new HTTPError(Status.INTERNAL_SERVER_ERROR, err);
     }
   }),
 };
